c35a16a9af8e25bcdaf75a3b60938c41e7bb0b8f
Theux94 <wichtigms@posteo.de>
2016-12-20 14:28:04 +0100

    checking if user is in database
    user!=db user
    email!= db email
    
    -> pw!=pwR

--- app/model/registermodel.js
+++ app/model/registermodel.js
@@ -3,9 +3,39 @@ var fs = require("fs");
 var file = "./app/model/DB_users.db";
 var exists = fs.existsSync(file);
 var db = new sqlite3.Database(file);
+var existsBool;
 
 function checkregister(req,res,email,user,password,logedIn){
-    db.run("INSERT INTO users VALUES (?,?,?,?)",email,user,password,logedIn,  function(err, rows){
+
+	db.all("SELECT * from users WHERE email=?",email, function(err, rows){
+		console.log("rows!!!:"+rows);
+			if (typeof rows[0] != 'undefined'){
+				existsBool=true;
+			}
+			else{
+				existsBool=false;
+				console.log("existboolsTRUE?"+existsBool)
+			}
+		});
+	console.log("WICHTIGG?"+existsBool)
+	
+	
+	db.all("SELECT * from users WHERE user=?",user, function(err, rows2){
+		console.log("rows2!!!:"+rows2);
+			if (typeof rows2[0] != 'undefined'){
+				existsBool=true;
+				console.log("richtig!"+existsBool);
+			}
+			else if (existsBool!=true){
+				existsBool=false;
+				console.log("existboolsTRUE?"+existsBool)
+			}
+		});
+	
+	
+	if (existsBool!=true){
+		console.log("exists???"+existsBool);
+    db.run("INSERT INTO users VALUES (?,?,?,?)",email,user,password,logedIn,  function(err, rows3){
 	if (err) 
 	    console.log(err);
 	else{ 
@@ -13,9 +43,13 @@ function checkregister(req,res,email,user,password,logedIn){
 		res.end("<p>NEW USER</p>");
 	    }
 	    
-	});
+	});}
+	else {
+		res.setHeader('Content-Type','text/html')
+		res.end("<p>Your E-Mail or User does already exists</p>");
+	}
 }
-
+/*
 function checkRegister(req,res,email,user,password,logedIn){
    
   console.log("DATABASE CONTENT:" + db.all("SELECT * FROM users"));
@@ -48,6 +82,6 @@ function checkRegister(req,res,email,user,password,logedIn){
      
  }
     });
-}
+}*/
      
 module.exports.checkregister = checkregister;
