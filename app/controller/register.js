97d51d137f407b5ddd2bd4a8cc7a02ea5478a8ff
Theux94 <wichtigms@posteo.de>
2016-12-20 14:30:01 +0100

    checking pw is the same as pwR

--- app/controller/register.js
+++ app/controller/register.js
@@ -4,10 +4,18 @@ var rm = require("../model/registermodel.js");
 class Register{
     //Checking Data, and the register or Login User
 
-    checkData(req,res){
-        
-	    rm.checkregister(req,res,req.post.email,req.post.username, req.post.password,"false");
-    }
+	checkData(req,res){ 
+
+if (req.post.password==req.post.passwordR){
+  rm.checkregister(req,res,req.post.email,req.post.username, req.post.password,"false");
+}
+else {
+  res.setHeader('Content-Type','text/html');
+  res.end("<p>You typed in two different passwords!</p>");
+	}
+}
+ 
+
 }
 
 module.exports.Register = Register;
